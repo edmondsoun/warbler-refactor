1081237b34eaede34ed13f7019b5cc15a1c3510d
Edmond Soun <edmond.soun@gmail.com>
2022-09-28 11:49:05 -0700

    introduced blueprint for root routes

---
+++ warbler/__init__.py
@@ -0,0 +1,93 @@
+
+import warbler.root.views # does this matter any more?
+
+import os
+from dotenv import load_dotenv
+from warbler import app #needed?
+
+from flask import (
+    Flask, render_template, request, flash, redirect, session, g, abort,
+)
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from .forms import LoginForm, CSRFProtection 
+
+from .models import (
+    db, connect_db, User, Message, DEFAULT_IMAGE_URL, DEFAULT_HEADER_IMAGE_URL)
+
+from .root import root
+
+load_dotenv()
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+# BEFORE:
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+@app.before_request
+def add_csrf_only_form():
+    """Add a CSRF-only form so that every route can use it."""
+
+    g.csrf_form = CSRFProtection()
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Log out user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+# ROUTES:
+app.register_blueprint(root)
+# app.register_blueprint(users)
+# app.register_blueprint(follows)
+# app.register_blueprint(messages)
+# app.register_blueprint(likes)
+
+# AFTER:
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 NOT FOUND page."""
+
+    return render_template('404.html'), 404
+
+
+@app.after_request
+def add_header(response):
+    """Add non-caching headers on every request."""
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
+    response.cache_control.no_store = True
+    return response
---
+++ warbler/views.py
@@ -0,0 +1,9 @@
+# Probably don't need top level views once blueprints is implemented.
+
+from . import app
+
+# @app.route('/')
+# def index():
+#     return 'Hello World!'
+
+
